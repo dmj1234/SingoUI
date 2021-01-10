70df7a4942200a09a051a2fdebd545a9ef244589
dmj1234 <1165689381@qq.com>
2021-01-10 10:19:56 +0800

    创建button group

--- src/button.vue
+++ src/button.vue
@@ -32,15 +32,9 @@ export default {
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 }
-.g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex; justify-content: center; align-items: center;
-  vertical-align: middle;
+.g-button {font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);background: var(--button-bg);
+  display: inline-flex; justify-content: center; align-items: center;vertical-align: middle;
   &:hover { border-color: var(--border-color-hover); }
   &:active { background-color: var(--button-active-bg); }
   &:focus { outline: none; }
