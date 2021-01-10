2577646067963c066e26257f4080d1666f5b0aff
dmj1234 <1165689381@qq.com>
2021-01-10 14:46:42 +0800

    添加 input.vue 及其样式

--- src/button.vue
+++ src/button.vue
@@ -14,6 +14,10 @@ import  Vue from 'vue'
 import  Icon from  './icon'
 Vue.component('g-icon',Icon)
 export default {
+  name: 'GuluButton',
+  components: {
+    'g-icon': Icon
+  },
   props: {
     icon: {},
     loading: {
@@ -30,7 +34,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @keyframes spin {
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
