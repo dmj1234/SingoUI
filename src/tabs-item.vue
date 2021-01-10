3870ca78d5411056dc49694a388747464da83ac9
dmj1234 <1165689381@qq.com>
2021-01-10 23:36:27 +0800

    增大tab item 可点击区域

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -45,7 +45,11 @@ export default {
 .tabs-item {
   flex-shrink: 0;
   padding: 0 1em;
-
+  cursor: pointer;
+  border: 1px solid #008000;
+  height: 100%;
+  display: flex;
+  align-items: center;
   &.active {
     background: red;
   }
