43267c30f61e73519647f63683075e8e7175cb57
dmj1234 <1165689381@qq.com>
2021-01-11 00:33:22 +0800

    添加插槽的 close API

--- src/popover.vue
+++ src/popover.vue
@@ -2,7 +2,7 @@
   <div class="popover" @click="onClick" ref="popover">
     <div class="popover" ref="popover">
          :class="{[`position-${position}`]:true}">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
@@ -13,6 +13,22 @@
 <script>
 export default {
   name: "GuluPopover",
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator (value) {
+        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      }
+    },
+    trigger: {
+      type: String,
+      default: 'click',
+      validator (value) {
+        return ['click', 'hover'].indexOf(value) >= 0
+      }
+    }
+  },
   data () {
     return {
       visible: false,
@@ -47,22 +63,6 @@ export default {
         return 'click'
       } else {
         return 'mouseleave'
-      }
-    }
-  },
-  props: {
-    position: {
-      type: String,
-      default: 'top',
-      validator (value) {
-        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
-      }
-    },
-    trigger: {
-      type: String,
-      default: 'click',
-      validator (value) {
-        return ['click', 'hover'].indexOf(value) >= 0
       }
     }
   },
