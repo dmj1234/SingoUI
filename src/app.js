8035a5d5b29ec3df9cc502b13a2835bde5d04483
dmj1234 <1165689381@qq.com>
2021-01-10 21:00:08 +0800

    实现进入动画，优化三种动画

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
@@ -10,6 +9,8 @@ import Header from './header'
 import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
@@ -21,20 +22,44 @@ Vue.component('g-header', Header)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
+import createElement from 'vue'
+import Vue from "vue";
+const h = createElement
 new Vue({
   el: '#app',
   data: {
     loading1: false,
     loading2: true,
     loading3: false,
-    message: 'Hello Word'
+    message: 'hi'
   },
   created(){
-
   },
   methods: {
-    inputChange(e) {
-      console.log(e)
+      showToast1(){
+        this.showToast('top')
+      },
+      showToast2(){
+        this.showToast('middle')
+      },
+      showToast3(){
+        this.showToast('bottom')
+      },
+      showToast(position){
+        this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+          position,
+          enableHtml: false,
+          closeButton: {
+            text: '已充值',
+            callback () {
+              console.log('他说已经充值智商了')
+            }
+          },
+          autoClose: false,
+          autoCloseDelay: 3
+        })
+      }
     }
-  }
-})
+  })
