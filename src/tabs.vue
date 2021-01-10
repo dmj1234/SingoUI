86f7caa2a725f100f60eaec78c8bd9b1578bbdfe
dmj1234 <1165689381@qq.com>
2021-01-11 00:12:15 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -15,36 +15,44 @@ export default {
     direction: {
       type: String,
       default: 'horizontal',
-      validator (value) {
+      validator(value) {
         return ['horizontal', 'vertical'].indexOf(value) >= 0
       }
     }
   },
-  data () {
+  data() {
     return {
       eventBus: new Vue()
     }
   },
-  provide () {
+  provide() {
     return {
       eventBus: this.eventBus
     }
   },
-  mounted () {
-    if (this.$children.length === 0) {
-      console && console.warn &&
-      console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === 'GuluTabsHead') {
-        vm.$children.forEach((childVm) => {
-          if (childVm.$options.name === 'GuluTabsItem'
-              && childVm.name === this.selected) {
-            this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console && console.warn &&
+        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
       }
-    })
+    },
+    selectTab () {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'GuluTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'GuluTabsItem'
+                && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    }
+  },
+  mounted () {
+    this.checkChildren();
+    this.selectTab();
   }
 }
 </script>
