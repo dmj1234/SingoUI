9ce28f7919ecca2614259e05f2a96f12625aa13d
dmj1234 <1165689381@qq.com>
2021-01-12 22:55:22 +0800

    完成基本样式

--- src/cascader.vue
+++ src/cascader.vue
@@ -2,6 +2,7 @@
   <div class="cascader">
 
       <div class="trigger" @click="popoverVisible = !popoverVisible">
+        {{result || '&nbsp;'}}
       </div>
     <div class="popover-wrapper" v-if="popoverVisible">
       <cascader-items :items="source" class="popover"
@@ -37,6 +38,11 @@ export default {
     onUpdateSelected (newSelected) {
       this.$emit('update:selected', newSelected)
     }
+  },
+  computed: {
+    result () {
+      return this.selected.map((item) => item.name).join('/')
+    }
   }
 }
 </script>
@@ -46,12 +52,17 @@ export default {
 .cascader {
   position: relative;
   .trigger {
-    height: 32px;
-    width: 100px;
-    border: 1px solid black;
+    height: $input-height;
+    display: inline-flex;
+    align-items: center;
+    padding: 0 1em;
+    min-width: 10em;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
   }
   .popover-wrapper {
     position: absolute; top: 100%; left: 0; background: white; display: flex;
+    margin-top: 8px;
     @extend .box-shadow;
   }
 }
